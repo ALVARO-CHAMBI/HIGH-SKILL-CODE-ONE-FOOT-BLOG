cb364316102046b1a03d705f8197b209e09cf2ce
unknown <chambia04@gmail.com>
2021-06-06 00:26:10 -0400

    primera parte foot blog

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './components/Card';
+
+
+function App() {
+    return (
+        <div className="container-cards">
+            {/* {data.map((card) => { */}
+            {/*     return <Card data={card} />; */}
+            {/* })} */}
+            <Card />
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+import './styles/Card.css'
+
+export default function card() {
+    return (
+        <>
+                <body>
+                    <section class="cards-container" id="app">
+                        <div class="card">
+                            <div class="hero-seation">
+                                <div class="menu-container">
+                                    <div class="menu-left">
+                                        FooBlo
+                                    </div>
+                                    <div class="menu-center">
+                                        <div class="menu-center-home">
+                                            Home
+                                        </div>
+                                        <div class="menu-center-recipes">
+                                            Recipes
+                                        </div>
+                                        <div class="menu-center-Pages">
+                                            Pages
+                                        </div>
+                                        <div class="menu-center-AboutMe">
+                                            About Me
+                                        </div>
+                                        <div class="menu-center-Blog">
+                                            Blog
+                                        </div>
+                                        <div class="menu-center-Contact">
+                                            Contact
+                                        </div>
+                                    </div>
+                                    <div class="menu-right">
+                                            <img src="https://i.servimg.com/u/f40/20/34/56/97/search10.jpg" alt="" id="lupa"/>
+                                        <div class="rectangulo">
+                                            <div class="rectangulo-text">
+                                                Hit enter to search
+                                            </div>
+                                        </div>
+                                    </div> 
+                                </div>
+                                <div class="card-Backgroud-Image">
+                                        <img src="https://i.servimg.com/u/f40/20/34/56/97/backgr11.jpg" alt=""/>
+                                        <div class="content-image-center">
+                                            <div class="card-texto">
+                                                Mexican Grilled <br/> Corn Recipe
+                                            </div>
+                                            <div class="card-tree-dates">
+                                                <div class="restaurant">
+                                                    <img src="https://i.servimg.com/u/f40/20/34/56/97/restau10.png" alt=""/>
+                                                </div>
+                                                <div class="restaurant-text" >
+                                                        4 Servicing
+                                                </div>
+                                                <div class="clock">
+                                                    <img src="https://i.servimg.com/u/f40/20/34/56/97/clock_10.png" alt=""/>
+                                                </div>
+                                                <div class="clock-text">
+                                                    40 Minute
+                                                </div>
+                                                <div class="user">
+                                                    <img src="https://i.servimg.com/u/f40/20/34/56/97/user_110.png" alt=""/>
+                                                </div>
+                                                <div class="user-text">
+                                                    Luice Fonci
+                                                </div>
+                                            </div>
+                                            <div class="card-view-recipes">
+                                                <div class="texto-view-recipes">
+                                                    View Recipes
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                                <div class="footer-imagens">
+                                    <div class="imguno">
+                                        <img src="https://i.servimg.com/u/f40/20/34/56/97/rectan10.png" alt=""/>
+                                    </div>
+                                    <div class="imgdos">
+                                        <img src="https://i.servimg.com/u/f40/20/34/56/97/rectan11.png" alt=""/>
+                                    </div>
+                                    <div class="imgtres">
+                                        <img src="https://i.servimg.com/u/f40/20/34/56/97/rectan12.png" alt=""/>
+                                    </div>
+                                    <div class="imgcuatro">
+                                        <img src="https://i.servimg.com/u/f40/20/34/56/97/rectan13.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    </section>
+                </body>
+
+        </>
+    )
+};
