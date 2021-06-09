662fc1ad7b92c1f604de6f442e8cbf6449eff922
Andreees <rommelgrillin@gmail.com>
2021-06-09 08:32:24 -0400

    parteandres

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,99 +1,100 @@
 import React from 'react';
-
+import Recetas from './Recetas';
+import Lateral from './Lateral';
 import './styles/Card.css'
 
 export default function card() {
     return (
         <>
-                <body>
-                    <section class="cards-container" id="app">
-                        <div class="card">
-                            <div class="hero-seation">
-                                <div class="menu-container">
-                                    <div class="menu-left">
+                
+                    <section className="cards-container" id="app">
+                        <div className="card">
+                            <div className="hero-seation">
+                                <div className="menu-container">
+                                    <div className="menu-left">
                                         FooBlo
                                     </div>
-                                    <div class="menu-center">
-                                        <div class="menu-center-home">
+                                    <div className="menu-center">
+                                        <div className="menu-center-home">
                                             Home
                                         </div>
-                                        <div class="menu-center-recipes">
+                                        <div className="menu-center-recipes">
                                             Recipes
                                         </div>
-                                        <div class="menu-center-Pages">
+                                        <div className="menu-center-Pages">
                                             Pages
                                         </div>
-                                        <div class="menu-center-AboutMe">
+                                        <div className="menu-center-AboutMe">
                                             About Me
                                         </div>
-                                        <div class="menu-center-Blog">
+                                        <div className="menu-center-Blog">
                                             Blog
                                         </div>
-                                        <div class="menu-center-Contact">
+                                        <div className="menu-center-Contact">
                                             Contact
                                         </div>
                                     </div>
-                                    <div class="menu-right">
+                                    <div className="menu-right">
                                             <img src="https://i.servimg.com/u/f40/20/34/56/97/search10.jpg" alt="" id="lupa"/>
-                                        <div class="rectangulo">
-                                            <div class="rectangulo-text">
+                                        <div className="rectangulo">
+                                            <div className="rectangulo-text">
                                                 Hit enter to search
                                             </div>
                                         </div>
                                     </div> 
                                 </div>
-                                <div class="card-Backgroud-Image">
+                                <div className="card-Backgroud-Image">
                                         <img src="https://i.servimg.com/u/f40/20/34/56/97/backgr11.jpg" alt=""/>
-                                        <div class="content-image-center">
-                                            <div class="card-texto">
+                                        <div className="content-image-center">
+                                            <div className="card-texto">
                                                 Mexican Grilled <br/> Corn Recipe
                                             </div>
-                                            <div class="card-tree-dates">
-                                                <div class="restaurant">
+                                            <div className="card-tree-dates">
+                                                <div className="restaurant">
                                                     <img src="https://i.servimg.com/u/f40/20/34/56/97/restau10.png" alt=""/>
                                                 </div>
-                                                <div class="restaurant-text" >
+                                                <div className="restaurant-text" >
                                                         4 Servicing
                                                 </div>
-                                                <div class="clock">
+                                                <div className="clock">
                                                     <img src="https://i.servimg.com/u/f40/20/34/56/97/clock_10.png" alt=""/>
                                                 </div>
-                                                <div class="clock-text">
+                                                <div className="clock-text">
                                                     40 Minute
                                                 </div>
-                                                <div class="user">
+                                                <div className="user">
                                                     <img src="https://i.servimg.com/u/f40/20/34/56/97/user_110.png" alt=""/>
                                                 </div>
-                                                <div class="user-text">
+                                                <div className="user-text">
                                                     Luice Fonci
                                                 </div>
                                             </div>
-                                            <div class="card-view-recipes">
-                                                <div class="texto-view-recipes">
+                                            <div className="card-view-recipes">
+                                                <div className="texto-view-recipes">
                                                     View Recipes
                                                 </div>
                                             </div>
                                         </div>
                                 </div>
-                                <div class="footer-imagens">
-                                    <div class="imguno">
+                                <div className="footer-imagens">
+                                    <div className="imguno">
                                         <img src="https://i.servimg.com/u/f40/20/34/56/97/rectan10.png" alt=""/>
                                     </div>
-                                    <div class="imgdos">
+                                    <div className="imgdos">
                                         <img src="https://i.servimg.com/u/f40/20/34/56/97/rectan11.png" alt=""/>
                                     </div>
-                                    <div class="imgtres">
+                                    <div className="imgtres">
                                         <img src="https://i.servimg.com/u/f40/20/34/56/97/rectan12.png" alt=""/>
                                     </div>
-                                    <div class="imgcuatro">
+                                    <div className="imgcuatro">
                                         <img src="https://i.servimg.com/u/f40/20/34/56/97/rectan13.png" alt=""/>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    
+                    <Recetas/>
+                    <Lateral/>
                     </section>
-                </body>
 
         </>
     )
