9881cc4117b7273058df7fffba955955d9690ed0
daniel54bhp <69922878+daniel54bhp@users.noreply.github.com>
2021-06-09 08:17:33 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import './global.css'
-
-const conteiner = document.getElementById('app');
-
-ReactDOM.render(<App />, conteiner);
